af25cbcee21e5591b7cd024f1eefa8b5ca0e68d7
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-07-27 18:05:50 +0530

    Applt createInquiry to the post method after apply save button which shows the query and sender informatin.

--- src/layouts/inquiry/data/inquiryData.js
+++ src/layouts/inquiry/data/inquiryData.js
@@ -63,6 +63,7 @@ export default function data() {
     "Ishan",
     "Pradip",
     "Akash",
+    "Nidhi",
   ]);
   const [searchField, setSearchField] = useState("");
   const handleTag = (e) => {
